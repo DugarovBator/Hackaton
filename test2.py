34e81474607f2628b339ad6739121491449d87c0
Konstantin Batorov <168391353+konstantinbatorov@users.noreply.github.com>
2025-07-03 11:44:18 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -28,6 +28,8 @@ player.add_animation("walk", [1, 7, 8, 9], fps=8, loop=True)
 player.add_animation("jump", [9, 10], fps=10, loop=False)
 # player.add_animation("stand", [64], fps=1, loop=False)
 player.add_animation("duck", [2, 3], fps=100, loop=False)
+player.add_animation("teleport1", [2], fps=10, loop=False)
+player.add_animation("teleport2", [3], fps=10, loop=False)
 
 player.set_scale(3.0)
 
@@ -41,7 +43,7 @@ wall = pg.AnimatedSprite("ground.png", (84, 21), (WIDTH / 2, HEIGHT / 2))
 wall.set_scale(3)
 
 # Ставим игрока на землю с учётом масштаба
-initial_ground_y = HEIGHT - (player.frame_size[1] * player.scale) / 2
+initial_ground_y = HEIGHT / 2 - (player.frame_size[1] * player.scale) / 2
 player.set_position(WIDTH // 2, initial_ground_y)
 player.play_animation("stance")
 
@@ -61,16 +63,17 @@ idle_timer = 0.0  # таймер покоя
 
 
 def player_update(dt):
+    global idle_timer, jump_speed
     keys = pygame.key.get_pressed()
 
     # Вычисляем границу земли и половину ширины на каждый кадр – учитываем масштаб
     half_w = (player.frame_size[0] * player.scale) / 2
     half_h = (player.frame_size[1] * player.scale) / 2
-    ground_y = HEIGHT - half_h
+    ground_y = HEIGHT / 2 - half_h
     
     # Стоим ли на земле?
     on_ground = player.y >= ground_y - 0.1
-
+    is_teleport = False
     # Приседаем
     if keys[pygame.K_s] and on_ground:
         player.play_animation("duck")
@@ -95,6 +98,7 @@ def player_update(dt):
                 player.play_animation("run" if keys[pygame.K_LSHIFT] else "walk")
             player.mirror(False)
 
+            
     # Логика покоя
     moving = keys[pygame.K_a] or keys[pygame.K_d]
     crouching = keys[pygame.K_s]
@@ -109,12 +113,30 @@ def player_update(dt):
     else:
         idle_timer = 0.0
 
+    if keys[pygame.K_q] and on_ground:
+        if player.y >= HEIGHT / 2 - half_h:
+            is_teleport = True
+            player.play_animation("teleport1")
+            ground_y = HEIGHT - half_h
+            player.y = player.y + HEIGHT / 2
+            player.play_animation("teleport2")
+            is_teleport = False
+    if keys[pygame.K_e] and on_ground:
+        if player.y < HEIGHT / 2 - half_h:
+            is_teleport = True
+            player.play_animation("teleport1")
+            ground_y = HEIGHT / 2 - half_h
+            player.y = player.y - HEIGHT / 2
+            player.play_animation("teleport2")
+            is_teleport = False
+
+    
     # Применяем гравитацию
     jump_speed += gravity * dt
     player.y += jump_speed * dt
 
     # Не проваливаемся сквозь землю
-    if player.y > ground_y:
+    if player.y > ground_y and not is_teleport:
         player.y = ground_y
         jump_speed = 0
 
@@ -123,7 +145,6 @@ def player_update(dt):
         player.x = half_w
     elif player.x > WIDTH - half_w:
         player.x = WIDTH - half_w
-
 # Главный цикл игры
 running = True
 while running:
